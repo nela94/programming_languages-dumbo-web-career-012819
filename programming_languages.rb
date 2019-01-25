0977ae98d64b5aed2702d2541425812bb65a7dfc
Manuela  Sanchez <m.sanchez947@hotmail.com>
2019-01-25 20:55:42 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,13 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |key, value|
+    value.each do |language, info|
+      if new_hash.key?(language)
+         new_hash[language][:style] << key
+      else
+          new_hash[language] = info.merge!({:style => [key]})
+      end
+    end
+  end
+      new_hash
 end
