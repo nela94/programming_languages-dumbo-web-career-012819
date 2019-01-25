1cc8ff02532d3e334ee99e24cfb9e038b491ef5e
Manuela  Sanchez <m.sanchez947@hotmail.com>
2019-01-25 20:54:26 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,5 +1,13 @@
 def reformat_languages(languages)
-  new_hash ={}
+  new_hash = {}
   languages.each do |key, value|
-end
+    value.each do |language, info|
+      if new_hash.key?(language)
+             new_hash[language][:style] << key
+      else
+            new_hash[language] = info.merge!({:style => [key]})
+      end
+    end
+  end
+      new_hash
 end
